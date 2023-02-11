e50047782bdf4f01142897bdac38bbfc62b28a26
Morgan Jackson <mbj4school@gmail.com>
2023-02-11 11:42:56 -0600

    data mapped into GML file

--- scraper.py
+++ scraper.py
@@ -1,29 +1,33 @@
 import requests
 import json
 import re
+import networkx as nx
 from bs4 import BeautifulSoup
 
 URL = "http://store.steampowered.com/explore/random/"
+G = nx.DiGraph()
 
-recList = []
+for _ in range(10000):
 
-page = requests.get(URL)
-soup = BeautifulSoup(page.text, 'html.parser')
-nameTag = soup.find("div", class_="apphub_AppName")
-idTag = soup.find("input", id="review_appid")
+  recList = []
 
-recommendations = re.search("{\"rgApps\".*", page.text)
-recString = recommendations.group(0)
-recString = recString.replace(");", "")
-recsDict = json.loads(recString)['rgApps']
+  page = requests.get(URL)
+  soup = BeautifulSoup(page.text, 'html.parser')
+  nameTag = soup.find("div", class_="apphub_AppName")
+  idTag = soup.find("input", id="review_appid")
 
-for i in range(5):
-    recList.append((recsDict[list(recsDict.keys())[i]]['name'], list(recsDict.keys())[i]))
+  recommendations = re.search("{\"rgApps\".*", page.text)
+  recString = recommendations.group(0)
+  recString = recString.replace(");", "")
+  recsDict = json.loads(recString)['rgApps']
 
-print(nameTag.text)
-print(idTag['value'])
-print(recList)
+  for i in range(5):
+      recList.append((recsDict[list(recsDict.keys())[i]]['name'], list(recsDict.keys())[i]))
 
+  print(nameTag.text)
+  #print(idTag['value'])
+  print(recList)
+  for rec in recList:
+      G.add_edge(nameTag.text, rec[0])
 
-with open('output.txt', 'w', encoding="utf-8") as f:
-    f.write(page.text)
+nx.write_gml(G, path="steamBig.gml")
