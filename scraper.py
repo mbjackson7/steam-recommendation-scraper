97d1e73433fa6d7923a22587192040200e9039e6
Morgan Jackson <morjack@siue.edu>
2023-02-11 12:15:01 -0600

    added timer for duration

--- scraper.py
+++ scraper.py
@@ -3,31 +3,39 @@ import json
 import re
 import networkx as nx
 from bs4 import BeautifulSoup
+import time
 
 URL = "http://store.steampowered.com/explore/random/"
 G = nx.DiGraph()
 
-for _ in range(10000):
+start = time.time()
 
-  recList = []
+for z in range(5000):
+    print(z)
+    recList = []
 
-  page = requests.get(URL)
-  soup = BeautifulSoup(page.text, 'html.parser')
-  nameTag = soup.find("div", class_="apphub_AppName")
-  idTag = soup.find("input", id="review_appid")
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    nameTag = soup.find("div", class_="apphub_AppName")
+    idTag = soup.find("input", id="review_appid")
 
-  recommendations = re.search("{\"rgApps\".*", page.text)
-  recString = recommendations.group(0)
-  recString = recString.replace(");", "")
-  recsDict = json.loads(recString)['rgApps']
+    recommendations = re.search("{\"rgApps\".*", page.text)
+    recString = recommendations.group(0)
+    recString = recString.replace(");", "")
+    recsDict = json.loads(recString)['rgApps']
 
-  for i in range(5):
-      recList.append((recsDict[list(recsDict.keys())[i]]['name'], list(recsDict.keys())[i]))
+    for i in range(5):
+        recList.append((recsDict[list(recsDict.keys())[i]]
+                       ['name'], list(recsDict.keys())[i]))
 
-  print(nameTag.text)
-  #print(idTag['value'])
-  print(recList)
-  for rec in recList:
-      G.add_edge(nameTag.text, rec[0])
+    #print(nameTag.text)
+    # print(idTag['value'])
+    #print(recList)
+    for rec in recList:
+        G.add_edge(nameTag.text, rec[0])
 
 nx.write_gml(G, path="steamBig.gml")
+
+end = time.time()
+
+print("Complete!\nFinished in: " + (end - start))
